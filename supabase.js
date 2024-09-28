df9b920e8342789655f92505ceb5b64ec72e9940
shamirul <hudashamirul@gmail.com>
2024-09-28 20:34:12 +0530

    supabase added

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL  // Replace with your Supabase URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY // Replace with your Supabase anon key
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
